c37a0f536d3d6ba643bc7be67df86be5b3a12462
Arif Babu <arif15-12904@diu.edu.bd>
2024-05-25 09:39:25 +0600

    This is first commit

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Post::create([  
+        //     'name' => fake()->unique()->name(),
+        //     'description' => 'cat-one',
+        // ]);
+    }
+}
